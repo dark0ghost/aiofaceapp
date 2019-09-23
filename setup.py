cca1db59e447a9f78a5dd0369b3d3795f2ad5e9c
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-09-23 22:46:43 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+requirements = [
+"aiohttp",
+"ujson"
+]
+
+setup(
+    name='aiofaceapp',
+    version='0.0.1',
+    description=" Python aioFaceApp.",
+    author="dark0ghost",
+    url='https://github.com/dark0ghost/aiofaceapp/',
+    packages=[
+        'aiofaceapp',
+    ],
+    package_dir={'aiofaceapp':
+                     'aiofaceapp'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache License 2.0",
+    zip_safe=False,
+    keywords='aiofaceapp, faceapp',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
